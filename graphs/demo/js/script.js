0056813e89bf6a48c771f3697601fce7b23d8485
Daniel Hernandez <daniel@degu.cl>
2013-12-08 09:28:42 -0300

    Adding a frontpage and a example

---
+++ graphs/demo/js/script.js
@@ -0,0 +1,18 @@
+/* Aquí va el script para generar la visualización. */
+
+sparql(
+    // Consulta
+    "SELECT distinct ?s ?p ?o " +
+    "WHERE { GRAPH <http://verdata.cl/graphs/demo> { ?s ?p ?o } } ",
+    // Acción
+    function(data) {
+	$.each(data.results.bindings, function(index, value) {
+	    $('table#named-graph-list').append(
+		$('<tr>')
+		    .append($('<td>').text(value.s.value))
+		    .append($('<td>').text(value.p.value))
+		    .append($('<td>').text(value.o.value))
+	    );
+	});
+    });
+
